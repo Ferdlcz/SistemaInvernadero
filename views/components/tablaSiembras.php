d3d0856905ea8ba4f1ada3925ce9e4bdfaa3cf7a
Ferdlcz <ferdlcskate@gmail.com>
2024-03-15 15:56:28 -0600

    tabla siembras corregida

--- views/components/tablaSiembras.php
+++ views/components/tablaSiembras.php
@@ -4,8 +4,8 @@
   <div class="table-responsive">
     <table class="table table-striped">
       <thead>
-        <tr>
-        <th>Nombre del dueño</th>
+        <tr class="text-center">
+          <th>Nombre del dueño</th>
           <th>Apellidos del dueño</th>
           <th>Variacion</th>
           <th>Número de charolas</th>
@@ -13,13 +13,14 @@
           <th>Número de invernadero</th>
           <th>Fecha aproximada de entrega</th>
           <th>Fecha de entrega</th>
-          <th>Acciones</th>
+          <th></th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
 
       <?php foreach ($plantaciones as $plantacion) : ?>
-                <tr>
+                <tr class="text-center">
                     <td><?php echo $plantacion['NombreDueño']; ?></td>
                     <td><?php echo $plantacion['ApellidosDueño']; ?></td>
                     <td><?php echo $plantacion['Variedad']; ?></td>
@@ -28,6 +29,8 @@
                     <td><?php echo $plantacion['NumeroInvernadero']; ?></td>
                     <td><?php echo $plantacion['FechaAproxEntrega']; ?></td>
                     <td><?php echo $plantacion['FechaEntrega']; ?></td>
+                    <td><button class="btn btn-danger">Eliminar</button></td>
+                    <td><button class="btn btn-primary">Actualizar</button></td>
                 </tr>
       <?php endforeach; ?>
 
